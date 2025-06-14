3930c63d57281844b0f28e548e3b931320e3951e
Raju18 <rajualaveni7893@gmail.com>
2025-06-14 11:14:01 +0530

    added resposive to nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -37,7 +37,7 @@ function NavBar(props) {
 
           {/* Menu icon only for mobile */}
           <div className="menuIcon" onClick={menubar}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white">
               <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
             </svg>
           </div>
